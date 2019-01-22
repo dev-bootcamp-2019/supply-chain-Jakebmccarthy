47e700eeafb5471c4a36bead310b69a58e7e17d8
McCarthy <jb.mccarthy@ucc.ie>
2019-01-22 00:34:29 +0000

    tests passed

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -2,6 +2,8 @@
     This exercise has been updated to use Solidity version 0.5
     Breaking changes from 0.4 to 0.5 can be found here: 
     https://solidity.readthedocs.io/en/v0.5.0/050-breaking-changes.html
+    
+    https://github.com/solidity-exercises/consensys-simple-bank-supply-chain/blob/master/supply_chain/contracts/SupplyChain.sol
 */
 
 pragma solidity ^0.5.0;
@@ -11,11 +13,17 @@ contract SupplyChain {
   /* set owner */
   address owner;
 
-  /* Add a variable called skuCount to track the most recent sku # */
+  /* Add a variable called skuCount to track the most recent sku # 
+  _________DONE____________
+  */
+  uint skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
+     _________DONE____________
   */
+  
+  mapping (uint => Item) public items;
 
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
@@ -23,22 +31,61 @@ contract SupplyChain {
     Shipped
     Received
     (declaring them in this order is important for testing)
+    _________DONE____________
   */
+  
+  enum State {ForSale, Sold, Shipped, Received}
 
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
+    _________DONE____________
   */
+  
+  struct Item {
+      string name;
+      uint sku;
+      uint256 price;
+      State state;
+      address payable seller;
+      address payable buyer;
+      
+  }
 
   /* Create 4 events with the same name as each possible State (see above)
-    Each event should accept one argument, the sku*/
+    Each event should accept one argument, the sku
+    _________DONE____________
+    */
+    
+    event ForSale(uint sku);
+    event Sold(uint sku);
+    event Shipped(uint sku);
+    event Received(uint sku);
 
-/* Create a modifer that checks if the msg.sender is the owner of the contract */
+/* Create a modifer that checks if the msg.sender is the owner of the contract 
+check back on this
+*/
+    modifier checkOwner() {
+        require(msg.sender == owner,"Caller is not owner" );
+        
+        _;
+    }
+    
+    
+    
+  modifier verifyCaller (address _address){
+      require (msg.sender == _address, "Caller not verified");
+      _;
+      
+  }
 
-  modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
-
-  modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
+  modifier paidEnough(uint _price){
+      require(msg.value >= _price, "Not paid enough");
+      _;
+      
+  }
+  
   modifier checkValue(uint _sku) {
     //refund them after pay for item (why it is before, _ checks for logic before func)
     _;
@@ -49,16 +96,49 @@ contract SupplyChain {
 
   /* For each of the following modifiers, use what you learned about modifiers
    to give them functionality. For example, the forSale modifier should require
-   that the item with the given sku has the state ForSale. */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+   that the item with the given sku has the state ForSale. 
+   _________DONE____________
+   */
+   
+  modifier forSale(uint _sku){
+      uint sku = _sku;
+      State state = items[sku].state;
+      require(state == State.ForSale, "The item state must be forSale");
+      _;
+  }
+  
+  
+  modifier sold(uint _sku){
+      uint sku = _sku;
+      State state = items[sku].state;
+      require(state == State.Sold, "The item state must be sold");
+      _;
+  }
+  
+  
+  
+  modifier shipped(uint _sku){
+      uint sku = _sku;
+      State state = items[sku].state;
+      require(state == State.Shipped, "The item state must be shipped");
+      _;
+  }
+  
+  
+  
+  modifier received(uint _sku){
+      uint sku = _sku;
+      State state = items[sku].state;
+      require(state == State.Received, "The item state must be received");
+      _;
+  }
 
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
+       owner = msg.sender;
+       skuCount = 0;
   }
 
   function addItem(string memory _name, uint _price) public returns(bool){
@@ -76,22 +156,42 @@ contract SupplyChain {
 
   function buyItem(uint sku)
     public
-  {}
+    payable
+    forSale(sku)
+    paidEnough(items[sku].price)
+    checkValue(sku)
+  {
+    items[sku].seller.transfer(items[sku].price);
+    items[sku].buyer = msg.sender;
+    items[sku].state = State.Sold;
+    emit Sold(sku);
+      
+  }
 
   /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
   is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
   function shipItem(uint sku)
     public
-  {}
+    sold(sku)
+    verifyCaller(items[sku].seller)
+  {
+      items[sku].state = State.Shipped;
+      emit Shipped(sku);
+  }
 
   /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
   is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
   function receiveItem(uint sku)
     public
-  {}
+    shipped(sku)
+    verifyCaller(items[sku].buyer)
+  {
+      items[sku].state = State.Received;
+      emit Received(sku);
+  }
 
-  /* We have these functions completed so we can run tests, just ignore it :) */
-  function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, uint state, address seller, address buyer) {
+  /* We have these functions completed so we can run tests, just ignore it :) REMOVE WHEN TEST */
+  function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
     name = items[_sku].name;
     sku = items[_sku].sku;
     price = items[_sku].price;
